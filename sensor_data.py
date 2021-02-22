0569769cf79e03fa613e19b4f93dd09f7e0b74be
r00be <78529968+r00be@users.noreply.github.com>
2021-02-22 17:36:13 +0100

    Update sensor_data.py

--- sensor_data.py
+++ sensor_data.py
@@ -29,29 +29,28 @@ def request_json(node_code):
 
 list_json = [] #list of the json dat (initialize)
 
-#while True:
-
 for i in  list_nodes:
 
-    node_code = i
-    json_data = request_json(node_code)
+  node_code = i
+  json_data = request_json(node_code)
 
-    #2. Construct the body of the response
-    id = json_data["lid"]
-    field = json_data["nome"]
-    vwc = rd.uniform(0,1)   #Volumetric Water Content: not present in the json, so it is a random number between 0 and 1
-    vwc = str(round(vwc, 2))
-    temp = json_data["temperatura"] 
-    temp_soil = (float(temp) +rd.uniform(-1.5, 1.5))  #not present in the json, so it is slightly bigger or smaller than the enviroment temperature
-    temp_soil = str(round(temp_soil, 2))
-    humidity = json_data["umidita_relativa"]
-    lat = json_data["lat"]
-    lon = json_data["lon"]
-    date = json_data["ora_unix"]
+  #2. Construct the body of the response
+  id = json_data["lid"]
+  field = json_data["nome"]
+  vwc = rd.uniform(0,1)   #Volumetric Water Content: not present in the json, so it is a random number between 0 and 1
+  vwc = str(round(vwc, 2))
+  temp = json_data["temperatura"] 
+  temp_soil = (float(temp) +rd.uniform(-1.5, 1.5))  #not present in the json, so it is slightly bigger or smaller than the enviroment temperature
+  temp_soil = str(round(temp_soil, 2))
+  humidity = json_data["umidita_relativa"]
+  lat = json_data["lat"]
+  lon = json_data["lon"]
+  date = json_data["ora_unix"]
 
-    x =  '{ "id":"'+id+'", "field":"'+field+'", "vwc":"'+str(vwc)+'", "temp":"'+temp+'", "temp_suolo":"'+str(temp_soil)+'", "humidity":"'+humidity+'", "lat":"'+lat+'", "lon":"'+lon+'", "date":"'+date+'"}'
-    y = json.loads(x)
+  x =  '{ "id":"'+id+'", "field":"'+field+'", "vwc":"'+str(vwc)+'", "temp":"'+temp+'", "temp_suolo":"'+str(temp_soil)+'", "humidity":"'+humidity+'", "lat":"'+lat+'", "lon":"'+lon+'", "date":"'+date+'"}'
+  y = json.loads(x)
 
-    list_json.append(y)
-    
-#time.sleep(10)
+  list_json.append(y)
+
+for x in list_json:
+  print(x)
