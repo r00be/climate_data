3c82b665bdbff2e7e192c601fdf48d4caae75e0b
r00be <78529968+r00be@users.noreply.github.com>
2021-03-21 22:03:03 +0100

    Update sensor_data.py

--- sensor_data.py
+++ sensor_data.py
@@ -1,56 +1,103 @@
 import random as rd
 import requests   #allows to send HTTP requests
 import json       #allows to work with JSON file
+import datetime as dt
+import time
 
-#codes of the "nodes" from the provinces' capitals of the Marche
 
-Ancona = 223
-Ascoli_Piceno = 373
-Fermo = 2743
-Macerata = 3733
-Pesaro = 5097
-Urbino = 7535
+field1 = [[1,100,13.71145248413086,43.158361065027506], 
+          [1,101,13.822174072265625,43.12604547425635], 
+          [1,102,13.4307861328125,43.16311928246929], 
+          [1,103,13.522796630859375,43.041794452901534],
+          [1,104,13.23028564453125,43.06186472916744]]
 
-list_nodes = [Pesaro,Urbino,Ancona,Macerata,Fermo,Ascoli_Piceno] #the codes are listed
+field2 = [[2,105,13.872985839843748,42.67233920753351], 
+          [2,106,13.7933349609375,42.48222557002593], 
+          [2,107,13.45001220703125,42.6844544397102]]
 
-#1. returns the json data of the nodes' code from the Corriere website
-def request_json(node_code):
+field3 = [[3,108,13.1671142578125,43.644025847699496], 
+          [3,109,12.859497070312498,43.42100882994726], 
+          [3,110,13.38134765625,43.39107786275974],
+          [3,111,13.172607421875,43.51270490464819]]
+  
 
-  payload = {}
-  headers = {}
-  URL = "https://meteo.corriere.it/meteoapi.php?c=" + str(node_code) #URL of the website
+#Genereting data
+def sensor_values():
+  date = dt.datetime.now()
+  epoch_time = time.time()
 
-  try:
-    response = requests.request("GET", URL, headers=headers, data=payload)
-    dataJson = json.loads(response.text) # response.text ==> Json (type Dictionary)
-    return dataJson
-  except:
-    print("ERRORE: connesione non riuscita")
+  if date.hour > 0 and date.hour < 4 or date.hour > 20:
+    temp = rd.uniform(0,8) 
+  else:
+    temp = rd.uniform(8,15) 
 
-list_json = [] #list of the json dat (initialize)
+  vwc = rd.uniform(0,1)   #Volumetric Water Content: numero randomico tra 0 e 1 (%)
+  temp_soil = (float(temp) + rd.uniform(0, 1.5))  #slightly bigger or smaller than the enviroment temperature
+  humidity = rd.uniform(0,100)
 
-for i in  list_nodes:
+  vwc = round(vwc, 2)
+  temp = round(temp, 2)
+  temp_soil = round(temp_soil, 2)
+  humidity = round(humidity, 2)
 
-  node_code = i
-  json_data = request_json(node_code)
+  list_values = [vwc,temp,temp_soil,humidity,epoch_time]
 
-  #2. Construct the body of the response
-  id = json_data["lid"]
-  field = json_data["nome"]
-  vwc = rd.uniform(0,1)   #Volumetric Water Content: not present in the json, so it is a random number between 0 and 1
-  vwc = str(round(vwc, 2))
-  temp = json_data["temperatura"] 
-  temp_soil = (float(temp) +rd.uniform(-1.5, 1.5))  #not present in the json, so it is slightly bigger or smaller than the enviroment temperature
-  temp_soil = str(round(temp_soil, 2))
-  humidity = json_data["umidita_relativa"]
-  lat = json_data["lat"]
-  lon = json_data["lon"]
-  date = json_data["ora_unix"]
+  return list_values
 
-  x =  '{ "id":"'+id+'", "field":"'+field+'", "vwc":"'+str(vwc)+'", "temp":"'+temp+'", "temp_suolo":"'+str(temp_soil)+'", "humidity":"'+humidity+'", "lat":"'+lat+'", "lon":"'+lon+'", "date":"'+date+'"}'
-  y = json.loads(x)
+def convert_to_json(list_values,i):
+  
+  x =  { "sensor_id": i[1], 
+         "volumetric_water_content":list_values[0], 
+         "temperature_enviroment": list_values[1], 
+         "temperature_soil": list_values[2], 
+         "percentage_humidity": list_values[3], 
+         "acquisition_time": list_values[4]
+        }
+  y = json.dumps(x)
 
-  list_json.append(y)
+  return y
 
-for x in list_json:
-  print(x)
+while True:
+
+  list_json = []
+
+  for i in  field1:
+    
+
+    list_values = sensor_values()
+    dataJson = convert_to_json(list_values,i)
+
+    list_json.append(dataJson)
+
+
+  for i in  field2:
+
+
+    list_values = sensor_values()
+    dataJson = convert_to_json(list_values,i)
+
+    list_json.append(dataJson)
+
+  for i in  field3:
+
+    list_values = sensor_values()
+    dataJson = convert_to_json(list_values,i)
+
+    list_json.append(dataJson)
+
+  for i in list_json:
+
+    payload = i
+    headers = {'content-type': 'application/json'}
+    
+    try:
+
+      URL = 'https://gcm2ijz2qa.execute-api.us-east-1.amazonaws.com/dev/fields/nodes'
+
+      response = requests.post(URL, payload, headers)  
+
+      print(response.text)
+    except:
+      print("failed request")
+
+  time.sleep(10)
